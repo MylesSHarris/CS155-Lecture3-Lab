c218e7d14c9eb9fbeead2c7d3fd958b6614b6e65
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-10-06 09:19:20 -0700

    commit

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string first = firstNameText.Text;
+            string last = lastNameText.Text;
+
+            string firstPiglatin = first.Substring(1, 1).ToUpper() + first.Substring(2) + first.Substring(0, 1).ToLower() + "ay";
+            string lastPiglatin = last.Substring(1, 1).ToUpper() + last.Substring(2) + last.Substring(0, 1).ToLower() + "ay";
+
+            MessageBox.Show(firstPiglatin + " " + lastPiglatin);
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool pluggedIn = true;
+            
+            if (pluggedIn)
+            {
+                bool burnedOut = true;
+
+                if (burnedOut)
+                {
+                    Console.WriteLine("Replace the bulb.");
+                }
+                else
+                {
+                    Console.WriteLine("Repair the lamp.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Plug in the lamp.");
+            }
+        }
+    }
+}
